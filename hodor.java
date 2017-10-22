7ee1e7abcb2adf984a520065c1b83199c2154821
shobhitbhatnagar150697 <shobhitbhatnagar150697@gmail.com>
2017-10-22 17:43:08 +0530

    My commit

---
+++ hodor.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class hodor
+{
+	static int a;
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("lund lele : ");
+		a=sc.nextInt();
+		System.out.println("lund : "+a);
+		
+	}
+}
